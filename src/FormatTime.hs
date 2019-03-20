74bb68f0ac834fb5fe10f1bf2610281c3f131108
Gareth Rowlands <gareth.rowlands@johnlewis.co.uk>
2019-03-20 00:29:24 +0000

    type signatures

--- src/FormatTime.hs
+++ src/FormatTime.hs
@@ -3,7 +3,9 @@ where
 
 import Data.List (find, unfoldr)
 
-timePeriods :: [(Integer, String)]
+type Period = String
+
+timePeriods :: [(Integer, Period)]
 timePeriods = [
         (year, "year"),
         (day, "day"),
@@ -23,16 +25,19 @@ formatTime 0 = "none"
 formatTime seconds = listify . map pluralise $ periods
     where periods = unfoldr findBiggestPeriod seconds
 
+findBiggestPeriod :: Integer -> Maybe ((Integer,Period),Integer)
 findBiggestPeriod seconds = do
     (n,period) <- find (\(n,_)->seconds>=n) timePeriods
     let (periodCount,remainder) = seconds `divMod` n
     return ((periodCount,period), seconds-n*periodCount)
 
+listify :: [String]->String
 listify [] = []
 listify [a] = a
 listify [a,b] = a ++ " and " ++ b
 listify (a:as) = a ++ ", " ++ listify as
 
+pluralise :: (Integer,String)->String
 pluralise (n,word)
     | n==1       = show n ++ " " ++ word
     | otherwise  = show n ++ " " ++ word ++ "s"
